2011e0687d2b8b7f5ba0127990836cf346287173
Moodboard-net <ramadhanw240@gmail.com>
2025-07-10 11:01:28 +0700

    1

--- config/Database.js
+++ config/Database.js
@@ -7,4 +7,12 @@ const db = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.D
   dialect: 'mysql'
 });
 
+db.authenticate()
+  .then(() => {
+    console.log('Connection has been established successfully.');
+  })
+  .catch((err) => {
+    console.error('Unable to connect to the database:', err);
+  });
+
 export default db;
